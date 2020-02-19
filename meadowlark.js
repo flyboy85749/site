c7518d1d645ed34e3b65164f106933ead4fd9629
William Hall <billhall168@gmail.com>
2020-02-18 18:53:28 -0700

    added some unit tests

--- meadowlark.js
+++ meadowlark.js
@@ -1,11 +1,10 @@
-const fortune = require('./lib/fortune')
-const express = require ('express');
+const express = require('express');
 const expressHandlebars = require('express-handlebars');
-
+const handlers = require('./lib/handlers')
 const app = express();
 
 // configure Handlebars view engine
-app.engine('handlebars', expressHandlebars ({
+app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main',
 }))
 
@@ -16,32 +15,23 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'))
 
 // home page route
-app.get('/', (req, res)=> {
-    res.render('home')
-})
+app.get('/', handlers.home)
 
 // about page route
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res)=> {
-    res.type('text/plain')
-    res.status(404)
-    res.send('404  Not Found')
-
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next)=> {
-    console.error(err.message)
-    res.type('text/plain')
-    res.status(500)
-    res.send('500 - Server Error')
-})
+app.use(handlers.serverError)
 
 
-app.listen(port, () => console.log(
-    `Express started on http://localhost:${port}; ' + ' press Ctrl-c to terminate.`
-))
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Express started on http://localhost:${port}` + 
+        ' press Ctrl-c to terminate.')
+    })
+} else {
+    module.exports = app
+}
